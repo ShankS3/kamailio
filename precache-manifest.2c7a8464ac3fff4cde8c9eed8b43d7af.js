e34a2c3197f2c91e055d060da8c05d0b1dfc4fe2
ShankS3 <shashank.surwase@gmail.com>
2018-11-10 13:56:20 +0530

    Updates

---
+++ precache-manifest.2c7a8464ac3fff4cde8c9eed8b43d7af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eab69f26f7f1e95758d110f0fa89e317",
+    "url": "/kamailio/static/media/museo-sans-500.eab69f26.woff"
+  },
+  {
+    "revision": "9c5ec994779f3fa49570",
+    "url": "/kamailio/static/js/runtime~main.9c5ec994.js"
+  },
+  {
+    "revision": "c9c0af2531b830a15327",
+    "url": "/kamailio/static/js/main.c9c0af25.chunk.js"
+  },
+  {
+    "revision": "ec49e04a74e28afd3fd8",
+    "url": "/kamailio/static/js/1.ec49e04a.chunk.js"
+  },
+  {
+    "revision": "c9c0af2531b830a15327",
+    "url": "/kamailio/static/css/main.03246a54.chunk.css"
+  },
+  {
+    "revision": "ec49e04a74e28afd3fd8",
+    "url": "/kamailio/static/css/1.44d710ae.chunk.css"
+  },
+  {
+    "revision": "3b57ebf41e9409a93aa114993268559c",
+    "url": "/kamailio/index.html"
+  }
+];
